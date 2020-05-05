728c9875a8f89545fbb257bc1193b3c5b7ef8d84
Cherkesky <guy@cherkesky.com>
2020-05-05 16:18:45 -0500

    asc/des in linq

--- Program.cs
+++ Program.cs
@@ -2,10 +2,66 @@
 using System.Linq;
 using System.Collections.Generic;
 
+
+public class Product
+{
+  /*
+  Properties
+  */
+  public string Title { get; set; }
+  public double Price { get; set; }
+
+  // Constructor method
+  public Product(string title, double price)
+  {
+    this.Title = title;
+    this.Price = price;
+  }
+}
+
 public class Program
 {
   public static void Main()
   {
+    /*
+               We can use curly braces to create instances of objects
+               and immediately inject them into the List.
+           */
+    List<Product> shoppingCart = new List<Product>(){
+            new Product("Bike", 109.99),
+            new Product("Mittens", 6.49),
+            new Product("Lollipop", 0.50),
+            new Product("Pocket Watch", 584.00)
+        };
+
+    /*
+              IEnumerable is an interface, which we'll get to later,
+              that we're using here to create a collection of Products
+              that we can iterate over.
+          */
+    IEnumerable<Product> inexpensive = from product in shoppingCart
+                                       where product.Price < 100.00
+                                       orderby product.Price descending
+                                       select product;
+    Console.WriteLine("**************************************");
+
+    foreach (Product p in inexpensive)
+    {
+      Console.WriteLine($"{p.Title} ${p.Price:f2}");
+    }
+
+    /*
+        You can also use `var` when creating LINQ collections. The
+        following variable will still be typed as List<Product> by
+        the compiler, but you don't need to type that all out.
+    */
+    var expensive = from product in shoppingCart
+                    where product.Price >= 100.00
+                    orderby product.Price descending
+                    select product;
+    Console.WriteLine("**************************************");
+
+
     /*
         Start with a collection that is of type IEnumerable, which
         List is and initialize it with some values. This is the
@@ -28,12 +84,11 @@ public class Program
                                   where count < 27 && count > 19
                                   orderby count ascending
                                   select count;
-   
+
     Console.WriteLine($"Average ideal size is {idealSizes.Average()}");
 
     // The @ symbol lets you create multi-line strings in C#
-    Console.WriteLine($@"
-    There were {idealSizes.Count()} ideally sized cohorts
+    Console.WriteLine($@"There were {idealSizes.Count()} ideally sized cohorts
     There have been {cohortStudentCount.Count()} total cohorts");
 
     // Display each number that was the acceptable size
